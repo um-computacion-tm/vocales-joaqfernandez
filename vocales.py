82c92e43eba99611a75cbe46d9c665775b6a709a
joaqfernandez <joal.fernandez@alumno.um.edu.ar>
2024-04-14 14:04:01 -0300

    elimino restantes innecesarios

--- vocales.py
+++ vocales.py
@@ -3,15 +3,7 @@ def contar_vocales(mi_string):
     resultado = {}
     for letra in mi_string:
         if letra in vocales:
-            if 'a' not in resultado:
+            if letra not in resultado:
                 resultado [letra] = 0
         resultado [letra] +=  1 
-        if letra == "e":
-            if 'e' not in resultado:
-                resultado ['e'] = 0
-            resultado ['e'] = resultado['e'] + 1
-    if contar_a > 0:
-        resultado ["a"] = contar_a
-    if contar_e > 0:
-        resultado ["e"] = contar_e
     return resultado
