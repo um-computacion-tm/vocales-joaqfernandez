df9139dd3810895a233ded5f2198ffe500a911bb
joaqfernandez <joal.fernandez@alumno.um.edu.ar>
2024-04-14 14:14:55 -0300

    agrego tests en mayusculas

--- tests.py
+++ tests.py
@@ -29,6 +29,30 @@ class TestContarVocales(unittest.TestCase):
 
     def test_contar_murciElago(self):
         resultado = contar_vocales("murciElago")
-        self.assertAlmostEqual(resultado,{"a": 3, "o" : 1})
-        
+        self.assertAlmostEqual(resultado,{"u": 1, "o" : 1, "a" : 1, "E" : 1, "i" : 1})
+#mayusculas
+    def test_contar_A(self):
+        resultado = contar_vocales("cAs")
+        self.assertAlmostEqual(resultado,{"A": 1})
+    def test_contar_AA(self):
+        resultado = contar_vocales("cAsA")
+        self.assertAlmostEqual(resultado,{"A": 2})
+    
+    def test_contar_bEsE(self):
+        resultado = contar_vocales("bEsE")
+        self.assertAlmostEqual(resultado,{"E": 2})
+
+    def test_contar_bEsA(self):
+        resultado = contar_vocales("bEsA")
+        self.assertAlmostEqual(resultado,{"A" : 1, "E" : 1 })
+
+    def test_contar_cAsAnOvA(self):
+        resultado = contar_vocales("cAsAnOvA")
+        self.assertAlmostEqual(resultado,{"A": 3, "O" : 1})
+
+    def test_contar_mUrcIElAgO(self):
+        resultado = contar_vocales("mUrcIElAgO")
+        self.assertAlmostEqual(resultado,{"U": 1, "O" : 1, "A" : 1, "E" : 1, "I" : 1})
+
+
 unittest.main()
