5c2d3cf5e8c30c5381ecb8cba025e1ed54f06024
joaqfernandez <joal.fernandez@alumno.um.edu.ar>
2024-04-14 14:10:13 -0300

    corrigo los test principales

--- tests.py
+++ tests.py
@@ -9,17 +9,26 @@ class TestContarVocales(unittest.TestCase):
 
     def test_contar_a(self):
         resultado = contar_vocales("cas")
-        self.assertAlmostEqual(resultado,{"a", 1})
+        self.assertAlmostEqual(resultado,{"a": 1})
 
     def test_contar_aa(self):
         resultado = contar_vocales("casa")
-        self.assertAlmostEqual(resultado,{"a", 2})
+        self.assertAlmostEqual(resultado,{"a": 2})
     
-    def test_contar_bc(self):
-        resultado = contar_vocales("casa")
-        self.assertAlmostEqual(resultado,{"a", 2})
+    def test_contar_bese(self):
+        resultado = contar_vocales("bese")
+        self.assertAlmostEqual(resultado,{"e": 2})
 
-    def test_contar_bc(self):
-        resultado = contar_vocales("casa")
-        self.assertAlmostEqual(resultado,{"a", 2})
+    def test_contar_besa(self):
+        resultado = contar_vocales("besa")
+        self.assertAlmostEqual(resultado,{"a" : 1, "e" : 1 })
+
+    def test_contar_casanova(self):
+        resultado = contar_vocales("casanova")
+        self.assertAlmostEqual(resultado,{"a": 3, "o" : 1})
+
+    def test_contar_murciElago(self):
+        resultado = contar_vocales("murciElago")
+        self.assertAlmostEqual(resultado,{"a": 3, "o" : 1})
+        
 unittest.main()
